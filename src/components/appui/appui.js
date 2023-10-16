d245cb0e335ef82d14652e980c7015056ddd337e
nabab <thomas.nabet@gmail.com>
2023-10-16 10:33:46 +0200

    Removed some subcomponents from appui for moving them in plugins

--- src/components/appui/appui.js
+++ src/components/appui/appui.js
@@ -315,69 +315,10 @@ return {
       if (this.mode === 'test') {
         return bbn._("Application in testing mode");
       }
-    },
-    powerMenu(){
-      if (!this.plugins || !this.plugins['appui-core'] || (!this.app?.user?.isAdmin && !this.app?.user?.isDev)) {
-        return [];
-      }
-
-      return [
-        {
-          action: () => {
-            this.confirm(
-              bbn._("Are you sure you want to delete the browser storage?"),
-              () => {
-                window.localStorage.clear();
-                this.$nextTick(() => {
-                  document.location.reload();
-                });
-              }
-            );
-          },
-          text: bbn._("Reload with a fresh view"),
-          icon: 'nf nf-mdi-sync_alert'
-        }, {
-          text: bbn._("Increase version"),
-          icon: 'nf nf-oct-versions',
-          action: () => {
-            bbn.fn.post(this.plugins['appui-core'] + '/service/increase').then(() => {
-              if (window.bbnSW) {
-                window.bbnSW.unregister().then(() => {
-                  document.location.reload();
-                });
-              }
-              else {
-                this.$nextTick(() => document.location.reload());
-              }
-            });
-          }
-        }
-      ];
-    },
-    powerColor(){
-      if (this.mode === 'dev') {
-        return 'var(--purple)';
-      }
-
-      if (this.mode === 'prod') {
-        return 'var(--green)';
-      }
-
-      if (this.mode === 'dev') {
-        return 'var(--blue)';
-      }
-
-      return '';
-    },
-    userMenu() {
-      return this.rightShortcuts;
     }
   },
   methods: {
     fdate: bbn.fn.fdate,
-    registerSearch() {
-      this.getRef('search').registerFunction(this.getRef('router').searchForString);
-    },
     onCopy(){
       let cpb = this.getRef('clipboardButton');
       //bbn.fn.log("AWATCH", cpb);
@@ -761,11 +702,6 @@ return {
           break;
       }
     },
-    searchSelect() {
-      this.$nextTick(() => {
-        this.searchOn = false;
-      })
-    },
     onLoad() {
       this.$emit('load', ...arguments);
 },
@@ -1138,120 +1074,6 @@ return {
     isLoading(newVal){
       if (!newVal) {
         this.loadingText = bbn._('Loading');
-      }
-    }
-  },
-  components: {
-    searchBar: {
-      name: 'search-bar',
-      props: {
-        source: {
-          type: Object,
-          default() {
-            return {}
-          }
-        }
-      },
-      template: `
-<bbn-autocomplete ref="search"
-                v-model="search"
-                :style="currentStyle"
-                v-bind="cfg"
-                v-on="eventsCfg"/>`,
-      data(){
-        return {
-          search: '',
-          style: this.source.style || {},
-          isExpanded: false
-        }
-      },
-      computed: {
-        ui(){
-          return this.closest('bbn-appui');
-        },
-        cfg(){
-          if ( this.source && Object.keys(this.source).length ){
-            let cfg = bbn.fn.extend(true, {}, this.source);
-            if ( cfg.focus !== undefined ){
-              delete cfg.focus;
-            }
-            if ( cfg.blur !== undefined ){
-              delete cfg.blur;
-            }
-            if ( cfg.change !== undefined ){
-              delete cfg.change;
-            }
-            if ( cfg.style !== undefined ){
-              delete cfg.style;
-            }
-            return cfg;
-          }
-          return {
-            delay: 500,
-            sourceText: 'text',
-            sourceValue: 'value',
-            clearButton: false,
-            suggest: true,
-            source: [],
-            placeholder: bbn._("Search anything..."),
-            icon: 'nf nf-fa-search',
-            minLength: 1,
-            height: bbn.env.height - 100,
-            template(d){
-              return `
-                <div class="bbn-hpadded bbn-nl">
-                  <div class="bbn-block-left">
-                    <h3>${d.text}</em></h3>
-                  </div>
-                  <div class="bbn-block-right bbn-h-100 bbn-r" style="display: table">
-                    <span style="display: table-cell; vertical-align: middle">${d.value}</span>
-                  </div>
-                </div>`;
-            }
-          };
-        },
-        eventsCfg(){
-          let def = {
-            focus: e => {
-              //bbn.fn.log("FOCUS");
-              if ( !this.isExpanded ){
-                let pane = this.closest('bbn-pane'),
-                    w = pane.$children[0].$el.clientWidth + pane.$children[1].$el.clientWidth - 40;
-                this.$refs.search.$refs.element.placeholder = this.cfg.placeholderFocused;
-                this.$set(this.style, 'width', w + 'px');
-                this.isExpanded = true;
-              }
-            },
-            blur: e => {
-              //bbn.fn.log("BLUR");
-              if ( this.isExpanded ){
-                this.$set(this.style, 'width', this.source.style && this.source.style.width ? this.source.style.width : '30px');
-                this.isExpanded = false;
-                this.$refs.search.$refs.element.placeholder = this.cfg.placeholder;
-                this.search = '';
-              }
-            },
-            change: id => {
-              if (id && !(id instanceof Event)) {
-                setTimeout(() => {
-                  document.activeElement.blur();
-                }, 15);
-              }
-            }
-          };
-          return {
-            focus: this.source.focus || def.focus,
-            blur: this.source.blur || def.blur,
-            change: this.source.change || def.change
-          };
-        },
-        currentStyle(){
-          return bbn.fn.extend({
-            'z-index': 10,
-            transition: 'width 400ms',
-            width: '30px'
-          }, (this.source.style || {}), this.style);
-        }
       }
     }
   }
