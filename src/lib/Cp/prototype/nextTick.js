0a2831dd9671d512c47a1c73967cd9d0630430fb
nabab <thomas.nabet@gmail.com>
2024-11-05 16:55:16 +0100

    Test nextTick

--- src/lib/Cp/prototype/nextTick.js
+++ src/lib/Cp/prototype/nextTick.js
@@ -1,20 +1,24 @@
+import queueUpdate from "../../../functions/queueUpdate.js";
 import bbnCp from "../Cp.js";
 
 bbnCp.prototype.$nextTick = async function(fn){
   const f = fn ? bbn.fn.analyzeFunction(fn) : {hash: 'nextTick'}; 
   const cp = this;
   return new Promise((resolve) => {
-    bbn.cp.nextQueue.push({
-      component: cp,
-      fn() {
-        let res;
-        if (fn) {
-          res = fn();
-        }
+    queueUpdate({cp, fn: () => {
+      bbn.cp.nextQueue.push({
+        component: cp,
+        fn() {
+          let res;
+          if (fn) {
+            res = fn();
+          }
+  
+          resolve(res);
+        },
+        hash: f.hash,
+      });
+    }})
 
-        resolve(res);
-      },
-      hash: f.hash,
-    });
   });
 }
