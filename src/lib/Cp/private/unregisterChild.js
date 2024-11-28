a2f7dfe9683070103c59c38f5584ce0e98d3f06a
nabab <thomas.nabet@gmail.com>
2024-11-28 17:25:38 +0100

    Bugfix

--- src/lib/Cp/private/unregisterChild.js
+++ src/lib/Cp/private/unregisterChild.js
@@ -4,16 +4,20 @@ import tryMount from "./tryMount.js";
  * Unregister the given child of the component from the $children array
  */
 export default function unregisterChild(child) {
-  let idx = child.$parent.$children.indexOf(child);
-  if (idx > -1) {
-    child.$parent.$children.splice(idx, 1);
+  if (child.$parent) {
+    let idx = child.$parent.$children.indexOf(child);
+    if (idx > -1) {
+      child.$parent.$children.splice(idx, 1);
+    }
   }
 
-  idx = child.$origin.$components.indexOf(child);
-  if (idx > -1) {
-    child.$origin.$components.splice(idx, 1);
-  }
-  if (!child.$origin.$isMounted) {
-    tryMount(child.$origin);
+  if (child.$origin) {
+    let idx = child.$origin.$components.indexOf(child);
+    if (idx > -1) {
+      child.$origin.$components.splice(idx, 1);
+    }
+    if (!child.$origin.$isMounted) {
+      tryMount(child.$origin);
+    }
   }
 }
