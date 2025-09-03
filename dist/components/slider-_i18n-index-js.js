a8486139bbfc52036fee1ad64281401d67c6d127
nabab <thomas.nabet@gmail.com>
2025-09-04 00:44:53 +0200

    Latest changes

--- dist/components/slider-_i18n-index-js.js
+++ dist/components/slider-_i18n-index-js.js
@@ -7,15 +7,12 @@
  * or disable the default devtool with "devtool: false".
  * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
  */
-(self["webpackChunk_bbn_bbn_cp"] = self["webpackChunk_bbn_bbn_cp"] || []).push([["components/slider-_i18n-index-js"],{
+(self["webpackChunk_bbn_bbn_cp"] = self["webpackChunk_bbn_bbn_cp"] || []).push([[42898],{
 
-/***/ "./src/components/slider/_i18n/index.js":
-/*!**********************************************************!*\
-  !*** ./src/components/slider/_i18n/index.js + 2 modules ***!
-  \**********************************************************/
+/***/ 74737:
 /***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
 
-eval("{// ESM COMPAT FLAG\n__webpack_require__.r(__webpack_exports__);\n\n// EXPORTS\n__webpack_require__.d(__webpack_exports__, {\n  \"default\": () => (/* binding */ _i18n)\n});\n\n;// ./src/components/slider/_i18n/slider.fr.lang\nconst slider_fr_namespaceObject = /*#__PURE__*/JSON.parse('{\"Impossible to get an orientation for the slider\":\"Impossbile d\\'avoir une orientation pour le slider\"}');\n;// ./src/components/slider/_i18n/slider.it.lang\nconst slider_it_namespaceObject = /*#__PURE__*/JSON.parse('{\"Impossible to get an orientation for the slider\":\"Impossibile ottenere un orientamento per lo slider\"}');\n;// ./src/components/slider/_i18n/index.js\n\n\n/* harmony default export */ const _i18n = ({\n  fr: slider_fr_namespaceObject,\n  it: slider_it_namespaceObject\n});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9zcmMvY29tcG9uZW50cy9zbGlkZXIvX2kxOG4vaW5kZXguanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7OztBQUFrQztBQUNBO0FBRWxDLDRDQUFlO0VBQ2JBLEVBQUU7RUFDRkMsRUFBRUEsRUFBQUEseUJBQUFBO0FBQ0osQ0FBQyIsInNvdXJjZXMiOlsid2VicGFjazovL0BiYm4vYmJuLWNwLy4vc3JjL2NvbXBvbmVudHMvc2xpZGVyL19pMThuL2luZGV4LmpzPzcxZjYiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IGZyIGZyb20gJy4vc2xpZGVyLmZyLmxhbmcnO1xuaW1wb3J0IGl0IGZyb20gJy4vc2xpZGVyLml0LmxhbmcnO1xuXG5leHBvcnQgZGVmYXVsdCB7XG4gIGZyLFxuICBpdFxufTtcbiJdLCJuYW1lcyI6WyJmciIsIml0Il0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./src/components/slider/_i18n/index.js\n\n}");
+eval("{// ESM COMPAT FLAG\n__webpack_require__.r(__webpack_exports__);\n\n// EXPORTS\n__webpack_require__.d(__webpack_exports__, {\n  \"default\": () => (/* binding */ _i18n)\n});\n\n;// ./src/components/slider/_i18n/slider.fr.lang\nconst slider_fr_namespaceObject = /*#__PURE__*/JSON.parse('{\"Impossible to get an orientation for the slider\":\"Impossbile d\\'avoir une orientation pour le slider\"}');\n;// ./src/components/slider/_i18n/slider.it.lang\nconst slider_it_namespaceObject = /*#__PURE__*/JSON.parse('{\"Impossible to get an orientation for the slider\":\"Impossibile ottenere un orientamento per lo slider\"}');\n;// ./src/components/slider/_i18n/index.js\n\n\n/* harmony default export */ const _i18n = ({\n  fr: slider_fr_namespaceObject,\n  it: slider_it_namespaceObject\n});//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiNzQ3MzcuanMiLCJtYXBwaW5ncyI6Ijs7Ozs7Ozs7Ozs7OztBQUFrQztBQUNBO0FBRWxDLDRDQUFlO0VBQ2JBLEVBQUU7RUFDRkMsRUFBRUEsRUFBQUEseUJBQUFBO0FBQ0osQ0FBQyIsInNvdXJjZXMiOlsid2VicGFjazovL0BiYm4vYmJuLWNwLy4vc3JjL2NvbXBvbmVudHMvc2xpZGVyL19pMThuL2luZGV4LmpzPzcxZjYiXSwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IGZyIGZyb20gJy4vc2xpZGVyLmZyLmxhbmcnO1xuaW1wb3J0IGl0IGZyb20gJy4vc2xpZGVyLml0LmxhbmcnO1xuXG5leHBvcnQgZGVmYXVsdCB7XG4gIGZyLFxuICBpdFxufTtcbiJdLCJuYW1lcyI6WyJmciIsIml0Il0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///74737\n\n}");
 
 /***/ })
 
