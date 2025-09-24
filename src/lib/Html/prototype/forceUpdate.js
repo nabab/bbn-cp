e0ee7cf3b1e3793f9182eea29f39c51c06b57705
nabab <thomas.nabet@gmail.com>
2025-09-24 13:17:44 +0200

    Latest changes

--- src/lib/Html/prototype/forceUpdate.js
+++ src/lib/Html/prototype/forceUpdate.js
@@ -2,6 +2,7 @@ import bbnProtoHtml from "../../Html/Proto.js";
 import initResults from "../private/initResults.js";
 
 bbnProtoHtml.$forceUpdate = async function (fn) {
+  return await bbn.cp.startTick();
   /*
   for (let n in this.$namespaces) {
     if (this.$namespaces[n] === 'computed') {
